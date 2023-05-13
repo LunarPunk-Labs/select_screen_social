fe0d9eda0acd2bdb10c1785a007231024370092c
Maija Grudule <34912439+MGrudule@users.noreply.github.com>
2023-05-13 21:26:45 +0200

    Initial commit

---
+++ components/Page/Content/Doc.vue
@@ -0,0 +1,30 @@
+<script setup>
+const props = defineProps({
+  emptyTip: {
+    type: String,
+    required: false,
+    default: 'This page is empty',
+  },
+})
+</script>
+
+<template>
+  <ContentDoc>
+    <template #default="{ doc }">
+      <PageHeader>
+        <PageTitle :text="doc.title" />
+      </PageHeader>
+      <PageBody>
+        <PageSection>
+          <ContentRenderer :value="doc" />
+        </PageSection>
+      </PageBody>
+    </template>
+    <template #empty>
+      <h1>{{ emptyTip }}</h1>
+    </template>
+    <template #not-found>
+      <Error :code="404" wrap />
+    </template>
+  </ContentDoc>
+</template>
---
+++ pages/post/[slug].vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'page',
+})
+</script>
+
+<template>
+  <PageWrapper class="flex flex-col">
+    <PageContentDoc empty-tip="Post im empty" />
+  </PageWrapper>
+</template>
